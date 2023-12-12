6e22da9a209ce005b65774c80d1753d274969604
Christopher McLoughlin <chris.mclg@gmail.com>
2023-12-12 11:48:24 +0000

    This is a test file

---
+++ TestCase.java
@@ -0,0 +1,12 @@
+public class TestCase
+{
+    public static void main(String[]args)
+    {
+        getI();
+    }
+
+    private static void getI() {
+        System.out.println("Hello World");
+        int i = 10;
+    }
+}
